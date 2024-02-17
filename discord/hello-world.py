940b0dfac51d2c8a0642797d206ab0c00983d1a8
Jordan Brudenell <nefkrystal@gmail.com>
2024-02-17 15:45:56 -0500

    move away from the 'bot' interface

--- discord/hello-world.py
+++ discord/hello-world.py
@@ -1,18 +1,33 @@
 import discord
-from discord.ext import commands
 
 # get token from parent directory
 import sys
 sys.path.append('..')
 from secret import discord_token
 
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        #if message.author.dm_channel is None:
+            #await message.author.create_dm()
+
+        if message.content.startswith('!hello'):
+            await message.author.dm_channel.send(
+                f"I saw your message at {message.created_at}, it was in the channel {message.channel}"
+            )
+            print(repr(message))
+
+
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-bot.run(discord_token)
+client = MyClient(intents=intents)
+client.run(discord_token)
