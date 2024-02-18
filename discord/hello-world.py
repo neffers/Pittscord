81f61ea913294430aeb324b6bb5f4ca00eaaf2e4
Jordan Brudenell <nefkrystal@gmail.com>
2024-02-17 20:04:57 -0500

    testing custom functionality for later integration

--- discord/hello-world.py
+++ discord/hello-world.py
@@ -5,25 +5,35 @@ import sys
 sys.path.append('..')
 from secret import discord_token
 
+# local server channel IDs
+bot_testing_channel_id = 1208576315070877706
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
-    async def on_message(self, message):
+        # Causes the bot to say "hello world" on load
+        # await self.say_hello()
+
+    async def on_message(self, message: discord.Message):
         # we do not want the bot to reply to itself
         if message.author.id == self.user.id:
             return
 
-        #if message.author.dm_channel is None:
-            #await message.author.create_dm()
+        if message.author.dm_channel is None:
+            await message.author.create_dm()
 
         if message.content.startswith('!hello'):
             await message.author.dm_channel.send(
                 f"I saw your message at {message.created_at}, it was in the channel {message.channel}"
             )
             print(repr(message))
+
+    async def say_hello(self):
+        bot_testing_channel = self.get_channel(bot_testing_channel_id)
+        await bot_testing_channel.send("Hello World!")
 
 
 intents = discord.Intents.default()
