472876d0aa62c88dc7dc38c63c1e806e223426b2
Jordan B <nefkrystal@gmail.com>
2024-02-23 18:14:53 -0500

    add docstrings to bot

--- bot.py
+++ bot.py
@@ -4,13 +4,13 @@ from discord.ext import commands
 from secret import discord_token
 
 intents = discord.Intents.all()
-intents.message_content = True
-
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 @bot.event
 async def on_ready():
+    """Runs when the bot is successfully logged in and ready to accept commands.
+    May also run after network failures? Don't use it to schedule things. (That's why there's a !sync command)"""
     print(f'We have logged in as {bot.user}')
 
 
@@ -19,11 +19,13 @@ async def on_ready():
 @app_commands.default_permissions(administrator=True)
 @app_commands.checks.has_permissions(administrator=True)
 async def identify(interaction: discord.Interaction, user: discord.User):
+    """Look up a user's Pitt ID. Currently only responds with Discord ID."""
     await interaction.response.send_message(f"{user.id}", ephemeral=True)
 
 
 @identify.error
 async def identify_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
+    """Runs when the identify function encounters some error."""
     if interaction.user.guild_permissions.administrator:
         response = "Encountered some unknown error! Check the logs for more."
         print(repr(error))
@@ -35,6 +37,7 @@ async def identify_error(interaction: discord.Interaction, error: app_commands.A
 
 @bot.command()
 async def sync(interaction: discord.Interaction):
+    """Command to re-register 'app_commands' (slash commands) with Discord so that they can be used."""
     print("Attempting sync")
     globalsync = await bot.tree.sync()
     localsync = await bot.tree.sync(guild=interaction.guild)
