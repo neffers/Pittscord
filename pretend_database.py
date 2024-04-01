42856c04728371b9baa7399d27a45a356b4a152b
Jordan Brudenell <nefkrystal@gmail.com>
2024-04-01 10:35:17 -0400

    update pretend database in line with real

--- pretend_database.py
+++ pretend_database.py
@@ -1,6 +1,7 @@
 class Database:
     def __init__(self, db_filename):
         self.students = {}
+        self.courses = []
 
     def add_student(self, pitt_id, discord_id):
         self.students[discord_id] = pitt_id
@@ -11,14 +12,16 @@ class Database:
     def remove_student_association(self, discord_id):
         self.students.pop(discord_id, None)
 
-    def add_semester_course(self, course_canvas_id, course_name, category_channel_id, recitation_react_message_id):
-        raise NotImplementedError
+    def add_semester_course(self, course_canvas_id, course_name, student_role_id, ta_role_id, category_channel_id,
+                            recitation_react_message_id):
+        self.courses.append((course_canvas_id, course_name, student_role_id, ta_role_id, category_channel_id,
+                             recitation_react_message_id))
 
     def get_semester_courses(self):
-        raise NotImplementedError
+        return self.courses
 
     def remove_semester_courses(self):
-        raise NotImplementedError
+        self.courses = []
 
     def add_course_recitation(self, course_canvas_id, recitation_name, reaction_id, associated_role_id):
         raise NotImplementedError
