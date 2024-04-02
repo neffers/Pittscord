0cb79bb50ef3c94f3c947a93ab23ddab012c3e47
Jordan Brudenell <nefkrystal@gmail.com>
2024-04-02 19:33:10 -0400

    starting to feel like a functional product

--- web.py
+++ web.py
@@ -1,26 +1,39 @@
-from quart import Quart
-
+from quart import Quart, render_template, session
 import grpc
+import json
 from rpc import Pittscord_ipc_pb2_grpc, Pittscord_ipc_pb2
 
+app = Quart(__name__)
 
-async def do_hello() -> str:
+app.secret_key = "secret"
+
+
+async def get_json_from_server(server_id):
     try:
         async with grpc.aio.insecure_channel('[::]:50051') as channel:
             stub = Pittscord_ipc_pb2_grpc.Pittscord_ipcStub(channel)
-            response = await stub.SayHello(Pittscord_ipc_pb2.HelloRequest())
-        return "Success"
+            response = await stub.GetJSON(Pittscord_ipc_pb2.JSONRequest(server_id=server_id))
+        return response.json
     except grpc.aio.AioRpcError:
-        return "Failure"
-
-
-app = Quart(__name__)
+        return None
 
 
 @app.route("/")
 async def default():
-    ret = await do_hello()
-    return ret
+    # In reality, get from a login
+    session['server'] = 1204258474851041330
+    return await render_template("example.html")
+
+
+@app.route("/config", methods=["POST"])
+async def recv_config():
+    return {}
+
+
+@app.route("/get_server_json")
+async def get_json():
+    ret = await get_json_from_server(session['server'])
+    return json.loads(ret)
 
 
 if __name__ == "__main__":
