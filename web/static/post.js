4eab6452811916df5c1b915893b4145aa9223cfa
Jordan Brudenell <nefkrystal@gmail.com>
2024-04-04 00:12:54 -0400

    delete files that will probably go unused

--- web/static/post.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-    document.getElementById("theButton").addEventListener("click", sendPost);
-}
-
-function sendPost() {
-    fetch("/post", { method: "post" });
-}
-
-window.addEventListener("load", setup);
